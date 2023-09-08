056e16f5a7397eeca1393de2f60419e07bce516b
Nelson Morales <nelsonenr@proton.me>
2023-09-08 13:14:29 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "qnum.h"
+
+extern qnum btoq(const char *nptr, int base);
+extern const char *qtob(qnum numb, int base);
+
+int main(int argc, char **argv)
+{
+	qnum n;
+	const char *s = argv[1];
+
+	if (argc != 4) {
+		fprintf(stderr, "Usage: qconv str base base\n");
+		exit(1);
+	}
+
+	n = btoq(s, atoi(argv[2]));
+	s = qtob(n, atoi(argv[3]));
+
+	puts(s);
+	exit(0);
+}
